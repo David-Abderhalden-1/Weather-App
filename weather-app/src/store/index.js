03a74b0425f1b27e8ce9cd0f472959d33773b783
David Abderhalden <david.abderhalden@swisscom.com>
2021-08-12 17:48:06 +0200

    Added API building function
    API can now be built and important functions are integrated in the store.

--- weather-app/src/store/index.js
+++ weather-app/src/store/index.js
@@ -2,6 +2,21 @@ import { createStore } from 'vuex'
 
 export const store = createStore({
     state: {
-        test: 'test'
-    }
+        apiRoute: 'https://api.openweathermap.org/data/2.5/onecall?',
+        apiKey: 'cdb3d2c6335926ea02db7e9f0393d50b',
+        api: null,
+    },
+    getters: {
+        getApi(state) {
+            return state.api
+        }
+    },
+    mutations: {
+        createAPI(state, payload) {
+            state.api = `${state.apiRoute}lat=${payload.lat}&lon=${payload.lon}&exclude=${payload.exclude}&appid=${state.apiKey}`
+        }
+    },
+    mounted() {
+        this.createAPI()
+    },
 })
--- weather-app/src/views/mainPage.vue
+++ weather-app/src/views/mainPage.vue
@@ -1,11 +1,33 @@
 <template>
  <p>mainpage</p>
+ <!-- FOR TESTING. REMOVE BEFORE DEPLOYMENT -->
+ <button @click="testLogApiBuild"/>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 
 export default {
-  name: 'App'
+  name: 'App',
+
+  computed: {
+    ...mapGetters({
+      api: 'getApi',
+    })
+  },
+
+  methods: {
+    // FOR TESTING
+    testLogApiBuild() {
+      this.$store.commit({
+        type: 'createAPI',
+        lat: '47.376888',
+        lon: '8.541694',
+        exclude: 'minutely,hourly,current',
+      })
+      console.log(this.api)
+    }
+  }
 }
 </script>
 
