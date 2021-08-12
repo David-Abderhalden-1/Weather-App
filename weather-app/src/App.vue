ba44b31fe2582416797eb2f996dbff62ddd5d110
David Abderhalden <david.abderhalden@swisscom.com>
2021-08-12 14:25:00 +0200

    Router befor Auto Add

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <p> {{ test }} </p>
+  <router-view></router-view>
   
 </template>
 
--- weather-app/src/main.js
+++ weather-app/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { store } from './store/index'
-/*
+
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from './components/mainPage'
 import LocationPage from './components/locationPage'
@@ -10,17 +10,17 @@ const routes = [
   { path:'/', component: MainPage},
   { path:'/location', component: LocationPage} // stadt in pfad evt
 ]
-*/
+
 const app = createApp(App)
 
 app.use(store)
 
-/*
+
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
 
 app.use(router)
-*/
+
 app.mount('#app')
