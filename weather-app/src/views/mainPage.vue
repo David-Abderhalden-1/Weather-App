e6e1b35356af39faa175e7d2638cfc04ec2276b0
taafolu5 <lukas.folch@swisscom.com>
2021-08-12 18:22:27 +0200

    started with main page

--- weather-app/src/views/mainPage.vue
+++ weather-app/src/views/mainPage.vue
@@ -1,13 +1,43 @@
 <template>
- <p>mainpage</p>
+  <div class="mainPage">
+    <div class="mainPage__container">
+      <input
+        class="mainPage__container__searchbar"
+        type="text"
+        placeholder="Search"
+      />
+      <button
+        class="mainPage__container__editBtn"
+        v-if="!inSearch"
+        @click="inSearch = true"
+      >
+        Edit
+      </button>
+      <button
+        class="mainPage__container__addBtn"
+        v-if="inSearch"
+        @click="inSearch = false"
+      >
+        Add
+      </button>
+    </div>
+    <div class="mainPage__card">
+      <p>placeholder div</p>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
-}
+  name: 'App',
+  data() {
+    return {
+      inSearch: false,
+    };
+  },
+};
 </script>
 
 <style>
+@import '../assets/view-mainPage.css';
 </style>
