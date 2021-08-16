8090b704d1031a6f3d027809a895f09f4666241e
Hänseler Joris <Joris.Haenseler@swisscom.com>
2021-08-16 16:38:13 +0200

    Implemented function to get current card to locationsPage

--- src/store/index.js
+++ src/store/index.js
@@ -4,35 +4,42 @@ export const store = createStore({
     state: {
         //excludeAll: ['minutely', 'hourly', 'current', 'daily'],     // all weather api 'exclude' param options
         //exclude: [],
+        currentCard: null,
+        searchLocationResponse: {}, // tmp search result storage
+        cards: [
+            //------------TESTDATEN---------------
+            {
+                title: 'Bern',
+                lat: 58.38,
+                lng: 86.84
+            }
+            //------------TESTDATEN---------------
+        ], // all active cards.
 
-        searchLocationResponse: {},         // tmp search result storage
-        cards: [],          // all active cards.
-
-// ---------------TEST DATA-------------------
-        cities: [
-          {
-            name: 'Zürich',
-            temperature: 26,
-            weather: 'cloudy',
-            lat: 57.38,
-            lon: 85.84
-          },
-          {
-            name: 'Bern',
-            temperature: 29,
-            weather: 'sunny',
-            lat: 58.38,
-            lon: 86.84
-          },
-          {
-            name: 'Genf',
-            temperature: 17,
-            weather: 'rain',
-            lat: 58.38,
-            lon: 86.84
-          },
-        ]
-// -------------------------------------------
+        // ---------------TEST DATA-------------------
+        cities: [{
+                    name: 'Zürich',
+                    temperature: 26,
+                    weather: 'cloudy',
+                    lat: 57.38,
+                    lon: 85.84
+                },
+                {
+                    name: 'Bern',
+                    temperature: 29,
+                    weather: 'sunny',
+                    lat: 58.38,
+                    lon: 86.84
+                },
+                {
+                    name: 'Genf',
+                    temperature: 17,
+                    weather: 'rain',
+                    lat: 58.38,
+                    lon: 86.84
+                },
+            ]
+            // -------------------------------------------
     },
     getters: {
         getSearchLocationResponse(state) {
@@ -41,17 +48,9 @@ export const store = createStore({
         getCards(state) {
             return state.cards
         },
-
-        // ----------TEST DATA -------
-        getLocationApi(state) {
-            return state.locationApi
-        },
-
-        
-        getCities(state){
-          return state.cities;
+        getCurrentCard(state) {
+            return state.cards[state.currentCard]
         }
-        // -------------------------
 
     },
     mutations: {
@@ -62,35 +61,37 @@ export const store = createStore({
 
         addCard(state) {
             // Top result is reformatted and stored as card
-            try{
+            try {
                 const location = state.searchLocationResponse[0]
                 const cardBuilder = {
-                    title: location.formatted,      // location preview name
-                    lat: location.geometry.lat,     // latitude
-                    lng: location.geometry.lng,     // longitude
-                }
-                // Prevent redundant cards
+                        title: location.formatted, // location preview name
+                        lat: location.geometry.lat, // latitude
+                        lng: location.geometry.lng, // longitude
+                    }
+                    // Prevent redundant cards
                 if (!state.cards.some(card => card["title"] === cardBuilder.title)) {
                     state.cards.push(cardBuilder)
+                } else {
+                    alert("card has already been added.") // probably change
                 }
-                else {
-                    alert("card has already been added.")       // probably change
-                }
-            }
-            catch (error){
+            } catch (error) {
                 return 0
             }
-            state.searchLocationResponse = {}          // reset the search results to null
+            state.searchLocationResponse = {} // reset the search results to null
         },
+
+        setCurrentCard(state, payload) {
+            state.currentCard = payload.cardId
+        }
     },
     actions: {
-      /*
-      getExcluded(state, payload) {
-        console.log(payload.include)
-        const index = state.excludeAll.indexOf('current')
-        state.exclude = state.excludeAll.splice(index)
-        return state.exclude
-      }
-      */
+        /*
+        getExcluded(state, payload) {
+          console.log(payload.include)
+          const index = state.excludeAll.indexOf('current')
+          state.exclude = state.excludeAll.splice(index)
+          return state.exclude
+        }
+        */
     }
 })
--- src/views/locationPage.vue
+++ src/views/locationPage.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <location-page-head></location-page-head>
+    <location-page-head :title="currentCard.title"></location-page-head>
     <location-page-hourly-forecast></location-page-hourly-forecast>
     <location-page-weekly-forecast></location-page-weekly-forecast>
   </main>
@@ -10,8 +10,19 @@
 import locationPageHead from "../components/locationPage-head.vue";
 import locationPageHourlyForecast from "../components/locationPage-hourlyforecast.vue";
 import locationPageWeeklyForecast from "../components/locationPage-weeklyforecast.vue";
+import { mapGetters } from 'vuex'
 export default {
   name: "App",
+
+  beforeCreate() {
+    this.$store.commit("setCurrentCard", { cardId: this.$route.params.id }); 
+  },
+
+  computed: {
+    ...mapGetters({
+      currentCard: "getCurrentCard",
+    }),
+  },
   components: {
     locationPageHead,
     locationPageHourlyForecast,
