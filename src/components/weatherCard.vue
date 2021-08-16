51f8a6f5cc627e3f5ea282425ff96e04225c1660
David Abderhalden <david.abderhalden@swisscom.com>
2021-08-16 19:24:41 +0200

    Active Icons on Status Code

--- src/components/weatherCard.vue
+++ src/components/weatherCard.vue
@@ -5,9 +5,14 @@
         {{ cityName }}
       </div>
       <div class="card__city-info">
-        <img class="city-info__img" v-if="cityWeather === 'cloudy'" src="@/assets/img/cloudy.svg" />
-        <img class="city-info__img" v-if="cityWeather === 'sunny'" src="@/assets/img/day.svg" />
-        <img class="city-info__img" v-if="cityWeather === 'rain'" src="@/assets/img/rainy-6.svg" />
+        <img class="city-info__img" v-if="weatherId == 804" src="@/assets/img/cloudy.svg" />
+        <img class="city-info__img" v-if="800 < weatherId < 804" src="@/assets/img/cloudy-day-3.svg" />
+        <img class="city-info__img" v-if="weatherId == 800" src="@/assets/img/day.svg" />
+        <img class="city-info__img" v-if="500 < weatherId < 532" src="@/assets/img/rainy-6.svg" />
+        <img class="city-info__img" v-if="weatherId == 500" src="@/assets/img/rainy-day.svg" />
+        <img class="city-info__img" v-if="599 < weatherId < 603 || 619 < weatherId < 623 " src="@/assets/img/snowy-6.svg" />
+        <img class="city-info__img" v-if="610 < weatherId < 617" src="@/assets/img/snowy-day.svg" />
+        <img class="city-info__img" v-if="199 < weatherId < 233" src="@/assets/img/thunder.svg" />
         {{ cityTemp }}°
       </div>
     </div>
@@ -16,7 +21,7 @@
 
 <script>
 export default {
-  props: ['cityName', 'cityTemp', 'cityWeather'],
+  props: ['cityName', 'cityTemp', 'weatherId'],
   name: 'App',
 };
 </script>
--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -35,7 +35,7 @@
     </div>
     <div class="main-page__container2">
         <div class="conteiner2__city-loop" v-for="(card, index) in cards" :key="index">
-          <weather-card class="city-loop__comp" :cityName="card.title" :cityTemp="requestTemperatur(index)" :cityWeather="requestWeather(index)"></weather-card>
+          <weather-card class="city-loop__comp" :cityName="card.title" :cityTemp="requestTemperatur(index)" :weatherId="requestWeather(index)"></weather-card>
           <button v-if="inEdit" @click="deleteCard(index)" class="city-loop__del-btn">--</button>
         </div>
     </div>
