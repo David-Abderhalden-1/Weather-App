e051f5d462cc5aff8e47acc2b92ce2e09d662624
David Abderhalden <david.abderhalden@swisscom.com>
2021-08-17 09:35:39 +0200

    Working Icons

--- src/components/weatherCard.vue
+++ src/components/weatherCard.vue
@@ -5,14 +5,15 @@
         {{ cityName }}
       </div>
       <div class="card__city-info">
+        <p v-if="weatherId == undefined">Bad Request</p>
         <img class="city-info__img" v-if="weatherId == 804" src="@/assets/img/cloudy.svg" />
-        <img class="city-info__img" v-if="800 < weatherId < 804" src="@/assets/img/cloudy-day-3.svg" />
+        <img class="city-info__img" v-if="weatherId < 804 && weatherId > 800" src="@/assets/img/cloudy-day-3.svg" />
         <img class="city-info__img" v-if="weatherId == 800" src="@/assets/img/day.svg" />
-        <img class="city-info__img" v-if="500 < weatherId < 532" src="@/assets/img/rainy-6.svg" />
+        <img class="city-info__img" v-if="weatherId < 532 && weatherId > 500" src="@/assets/img/rainy-6.svg" />
         <img class="city-info__img" v-if="weatherId == 500" src="@/assets/img/rainy-day.svg" />
-        <img class="city-info__img" v-if="599 < weatherId < 603 || 619 < weatherId < 623 " src="@/assets/img/snowy-6.svg" />
-        <img class="city-info__img" v-if="610 < weatherId < 617" src="@/assets/img/snowy-day.svg" />
-        <img class="city-info__img" v-if="199 < weatherId < 233" src="@/assets/img/thunder.svg" />
+        <img class="city-info__img" v-if="(weatherId < 603 && weatherId > 599) || (weatherId < 623 && weatherId > 619)" src="@/assets/img/snowy-6.svg" />
+        <img class="city-info__img" v-if="weatherId < 617 && weatherId > 610" src="@/assets/img/snowy-day.svg" />
+        <img class="city-info__img" v-if="weatherId < 233 && weatherId > 199" src="@/assets/img/thunder.svg" />
         {{ cityTemp }}°
       </div>
     </div>
